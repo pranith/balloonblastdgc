2f8be978b1d4086a1a3cb1d8703eb3aef3dfdc20
bobby.prani@gmail.com <bobby.prani@gmail.com@f77e3e54-ed24-0534-ae84-a5e511dbf661>
2011-04-22 09:43:54 +0000

    working a bit...

--- trunk/src/com/dgcgames/balloonblast/World.java
+++ trunk/src/com/dgcgames/balloonblast/World.java
@@ -1,5 +1,9 @@
 package com.dgcgames.balloonblast;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class World {
     public interface WorldListener {
         public void release();
@@ -16,13 +20,20 @@ public class World {
         public static final int WORLD_STATE_NEXT_LEVEL = 1;
         public static final int WORLD_STATE_GAME_OVER = 2;
         
+        public final Random rand;
+        
         public int balloonsMissed;
         public int score;
         public int state;
+        
+        public final List<Balloon> balloons;
     
     public World(WorldListener listener) {
     	
+    	rand = new Random();
     	this.balloonsMissed = 0;
+    	this.balloons = new ArrayList<Balloon>();
+        generateLevel();
 /*        this.bob = new Bob(5, 1);
         this.platforms = new ArrayList<Platform>();
         this.springs = new ArrayList<Spring>();
@@ -30,7 +41,7 @@ public class World {
         this.coins = new ArrayList<Coin>();
         this.listener = listener;
         rand = new Random();
-        generateLevel();
+
 
         this.heightSoFar = 0;
         this.score = 0;
@@ -38,6 +49,7 @@ public class World {
     }
     
     public void update(float deltaTime, float accelX) {
+    	updateBalloon(deltaTime);
 /*        updateBob(deltaTime, accelX);
         updatePlatforms(deltaTime);
         updateSquirrels(deltaTime);
@@ -47,6 +59,21 @@ public class World {
         checkGameOver();
     }
     
+    private void updateBalloon(float deltaTime) {
+		for (int i = 0; i < balloons.size(); i++) {
+			Balloon balloon = balloons.get(i);
+			balloon.update(deltaTime);
+		}
+		
+	}
+
+	public void generateLevel() {
+		for (int i = 0; i < 20; i++)
+		{
+			Balloon balloon = new Balloon(50, i);
+			balloons.add(balloon);
+		}
+    }
     private void checkGameOver() {
         if (balloonsMissed >= 25) {
             state = WORLD_STATE_GAME_OVER;

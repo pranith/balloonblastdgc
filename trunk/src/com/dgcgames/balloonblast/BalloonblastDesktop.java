28e18bcb10b9be4869ed459f15f6e794a4975b40
bobby.prani@gmail.com <bobby.prani@gmail.com@f77e3e54-ed24-0534-ae84-a5e511dbf661>
2011-04-22 07:33:27 +0000

    main instantation

--- trunk/src/com/dgcgames/balloonblast/BalloonblastDesktop.java
+++ trunk/src/com/dgcgames/balloonblast/BalloonblastDesktop.java
@@ -1,12 +1,14 @@
 package com.dgcgames.balloonblast;
 
+import com.badlogic.gdx.backends.jogl.JoglApplication;
+
 public class BalloonblastDesktop {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new JoglApplication(new Balloonblast(), "My Game", 960, 600, false);
 
 	}
 

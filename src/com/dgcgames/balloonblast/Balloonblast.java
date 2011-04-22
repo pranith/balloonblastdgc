6e16f7a9f2fe4b2136211ae6e5b7334158576b05
bobby.prani@gmail.com <bobby.prani@gmail.com@f77e3e54-ed24-0534-ae84-a5e511dbf661>
2011-04-22 06:58:04 +0000

    asset load and minor changes

--- src/com/dgcgames/balloonblast/Balloonblast.java
+++ src/com/dgcgames/balloonblast/Balloonblast.java
@@ -1,5 +1,16 @@
 package com.dgcgames.balloonblast;
 
 public class Balloonblast extends Game {
-
+	boolean firstTimeCreate = true;
+	@Override
+    public Screen getStartScreen() {
+        return new MainMenuScreen(this);
+    }
+    
+    @Override
+    public void create() {                         
+         Settings.load();
+         Assets.load();
+         super.create();
+    } 
 }

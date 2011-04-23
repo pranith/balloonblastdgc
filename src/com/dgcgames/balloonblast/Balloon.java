c4fd841eacce3a476bd65df3a267829b72da2571
i@jigarsavla.com <i@jigarsavla.com@f77e3e54-ed24-0534-ae84-a5e511dbf661>
2011-04-23 23:35:49 +0000

    modified the velocity of the balloon, the draw function in WorldRenderer.java needs to be changed if rotation needs to be taken into account. Acceleration is not working

--- src/com/dgcgames/balloonblast/Balloon.java
+++ src/com/dgcgames/balloonblast/Balloon.java
@@ -6,9 +6,10 @@ public class Balloon extends DynamicGameObject{
 	    public static final float BALLOON_WIDTH = 0.5f;
 	    public static final float BALLOON_HEIGHT = 0.8f;
 	    public static final int BALLOON_SCORE = 10;
-	    public static final float BALLOON_VELOCITY = 3f;
-	    public static final float BALLOON_ACCELARATION_X = 0;
-	    public static final float BALLOON_ACCELARATION_Y = 0;
+	    public static final float BALLOON_VELOCITY_X = 2f;
+	    public static final float BALLOON_VELOCITY_Y = -0.2f;
+	    public static final float BALLOON_ACCELARATION_X = 10f;
+	    public static final float BALLOON_ACCELARATION_Y = -10f;
 	    
 	    public boolean BALLOON_STATE_HIT = false;
 
@@ -16,22 +17,22 @@ public class Balloon extends DynamicGameObject{
 	    
 	    public Balloon(float x, float y) {
 	        super(x, y, BALLOON_WIDTH, BALLOON_HEIGHT);
-	        velocity.set(BALLOON_VELOCITY, 0);
+	        velocity.set(BALLOON_VELOCITY_X, BALLOON_VELOCITY_Y);
 	        accel.set(BALLOON_ACCELARATION_X, BALLOON_ACCELARATION_Y);
 	    }
 	    
 	    public void update(float deltaTime) {
-	        position.add(velocity.x * deltaTime, velocity.y * deltaTime);
+	        position.add( (float) (velocity.x * deltaTime + 0.5*accel.x*java.lang.Math.pow ( deltaTime, 2) ), (float) (velocity.y * deltaTime + 0.5*accel.y*deltaTime*deltaTime) );
 	        bounds.x = position.x - BALLOON_WIDTH / 2;
 	        bounds.y = position.y - BALLOON_HEIGHT / 2;
 	        
 	        if(position.x < BALLOON_WIDTH / 2 ) {
 	            position.x = BALLOON_WIDTH / 2;
-	            velocity.x = BALLOON_VELOCITY;
+	            velocity.x = BALLOON_VELOCITY_X;
 	        }
 	        if(position.x > World.WORLD_WIDTH - BALLOON_WIDTH / 2) {
 	            position.x = World.WORLD_WIDTH - BALLOON_WIDTH / 2;
-	            velocity.x = -BALLOON_VELOCITY;
+	            velocity.x = -BALLOON_VELOCITY_X;
 	        }
 	        stateTime += deltaTime;
 	    }
